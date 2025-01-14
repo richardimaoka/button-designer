ce40d299de8f4e40b2b19c22e4198f8aed6440dd
richardimaoka <richard.s.imaoka@gmail.com>
2025-01-15 03:59:59 +0900

    centering button

---
+++ nextjs/app/components/official/text-only/Button.tsx
@@ -0,0 +1,7 @@
+import styles from "./Button.module.css";
+
+type Props = {};
+
+export function Button(props: Props) {
+  return <button className={styles.component}>button</button>;
+}
--- nextjs/app/components/ui/main/Main.tsx
+++ nextjs/app/components/ui/main/Main.tsx
@@ -1,5 +1,10 @@
+import { Button } from "../../official/text-only/Button";
 import styles from "./Main.module.css";
 
 export function Main() {
-  return <div className={styles.component}></div>;
+  return (
+    <div className={styles.component}>
+      <Button />
+    </div>
+  );
 }
